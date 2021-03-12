91ecff4ad23da51ec8b7b0a092ca40d450f28008
Victor Quintela <victorquintela10@gmail.com>
2021-03-11 21:19:27 -0300

    carrito merpag y nodemailer

---
+++ src/lib/mercadopago.js
@@ -0,0 +1,45 @@
+const mercadopago = require ('mercadopago');
+const { mercadoPagoKeys } = require('./keys');
+
+mercadopago.configure({
+    access_token: mercadoPagoKeys.accesToken
+});
+
+const generarPago = async (carrito, factura, user) => {
+    let itemsMP = [];
+    carrito.forEach(element => {
+        const item = {
+            id: element.item._id,
+            title: element.item.nombre,
+            category_id: `${element.item.id_prod_cat}`,
+            quantity: parseInt(element.qty), 
+            currency_id: 'ARS',
+            unit_price: element.item.precio
+        }
+        itemsMP.push(item)
+    });
+    let preference = {
+        items: itemsMP,
+        payer: {
+            name: user.nombre,
+            surname: user.apellido,
+            email: user.email,
+        },
+        external_reference: `${factura}`,
+        back_urls: {
+            "success": "http://localhost:3000/venta/success",
+            "failure": "http://localhost:3000/venta/failure",
+            "pending": "http://localhost:3000/venta/pending"
+        },
+        auto_return: "approved"
+    };
+    try {
+        const response = await mercadopago.preferences.create(preference);
+        initPoint = response.body.init_point;
+        return initPoint;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { generarPago };
